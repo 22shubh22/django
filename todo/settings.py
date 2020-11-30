c87114802ba0f10689a244c7a9d05eebba2129e4
22shubh22 <22shubh22@gmail.com>
2020-11-30 07:29:40 +0530

    resolve build conflict

--- todo/settings.py
+++ todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,5 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
